9f66f9254d7827f146f6dcc1f539d936ce4c74f9
Juan Gomez <gomex086@gmail.com>
2020-12-01 16:40:19 -0500

    Agregando archivos pre procesado y limpio

---
+++ pre-procesamiento.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('covid-pre-procesado.csv')
+new_df = pd.DataFrame(df)
+new_df = new_df.dropna()
+
+new_df.to_csv('covid-limpio.csv',index=False)
